ac3dede30a8f410ef2c7b803425119f9f473cde2
Mana Misumi <mana.misumi3050@gmail.com>
2023-11-18 19:14:52 +1100

    build app

--- image-search-app/src/App.js
+++ image-search-app/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import ImageGallery from './ImageGallery';
+import {useRef, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [fetchData, setFetchData] = useState([]);
+  const ref = useRef();
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    console.log(ref.current.value);
+
+    // URL for API
+    const endpointURL =`https://pixabay.com/api/?key=40744009-556e92046e1377f65ada9324a&q=${ref.current.value}&image_type=photo`;
+    fetch(endpointURL)
+      .then((res) => {
+      return res.json();
+    })
+      .then((data) => {
+        console.log(data.hits);
+        setFetchData(data.hits);
+      });
+  };
+
+    return (
+  <div className="container">
+    <h2>My Pixabay</h2>
+    <form onSubmit={(e) => handleSubmit(e)}>
+      <input type="text" placeholder="Search images here" ref={ref}/>
+    </form>
+    <ImageGallery fetchData={fetchData}/>
+  </div>);
 }
 
 export default App;
